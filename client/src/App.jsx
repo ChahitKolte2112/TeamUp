131271492bcd27d201fcb3975ebcdb058ebd9721
ChahitKolte <chahitkolte@gmail.com>
2023-06-25 20:21:48 +0530

    Intialization of the Project

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import SignUp from "../pages/SignUp";
+import Login from "../pages/Login";
+function App() {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/sign-up" element={<SignUp />} />
+                    <Route path="/log-in" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+}
+
+export default App;
